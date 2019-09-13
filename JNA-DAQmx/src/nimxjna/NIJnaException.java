6d53720e17a50bed62f144860700c166d458aec0
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2019-09-13 10:51:17 +0000

    Sharing JNA-DAQmx project into a new repository

---
+++ JNA-DAQmx/src/nimxjna/NIJnaException.java
@@ -0,0 +1,14 @@
+package nimxjna;
+
+public class NIJnaException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param message
+	 */
+	public NIJnaException(String message) {
+		super(message);
+	}
+
+}
